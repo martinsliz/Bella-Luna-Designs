3773f38b14453ad013c8fe66febf6b4fd78961c0
Liz Martins <elizabeth.martins5@gmail.com>
2023-03-26 12:52:22 -0400

    home component

---
+++ client/src/App.js
@@ -0,0 +1,15 @@
+import React from 'react'
+import Home from './pages/Home'
+import './App.css'
+
+const App = () => {
+  return (
+    <div>
+      <main>
+        <Home />
+      </main>
+    </div>
+  )
+}
+
+export default App
---
+++ client/src/pages/Home.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+
+const Home = () => {
+  return (
+    <div>
+      <div>
+        <h1>Bella Luna Designs</h1>
+      </div>
+    </div>
+  )
+}
+
+export default Home
